700dc199395fd96e68c27a4122b911d783666c74
JungSonga <wwwa@naver.com>
2017-08-04 11:21:28 +0900

    PROJECT START

---
+++ collection view/Task.swift
@@ -0,0 +1,19 @@
+//
+//  Task.swift
+//  Todobox
+//
+//  Created by 전수열 on 12/26/15.
+//  Copyright © 2015 Suyeol Jeon. All rights reserved.
+//
+
+struct Task {
+  var title: String
+  var note: String?
+  var done: Bool = false
+
+  init(title: String, note: String? = nil, done: Bool = false) {
+    self.title = title
+    self.note = note
+    self.done = done
+  }
+}
---
+++ collection view/TaskEditorViewController.swift
@@ -0,0 +1,80 @@
+//
+//  TaskEditorViewController.swift
+//  Todobox
+//
+//  Created by 전수열 on 12/26/15.
+//  Copyright © 2015 Suyeol Jeon. All rights reserved.
+//
+
+import UIKit
+
+class TaskEditorViewController: UIViewController {
+
+  @IBOutlet var titleInput: UITextField!
+  @IBOutlet var textView: UITextView!
+
+  var didAddHandler: ((Task) -> Void)?
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    self.textView.layer.cornerRadius = 5
+    self.textView.layer.borderColor = UIColor.lightGray.cgColor
+    self.textView.layer.borderWidth = 1 / UIScreen.main.scale
+  }
+
+  override func viewWillAppear(_ animated: Bool) {
+    super.viewWillAppear(animated)
+    self.titleInput.becomeFirstResponder()
+  }
+
+  @IBAction func cancelButtonDidTap() {
+    self.titleInput.resignFirstResponder()
+
+    if self.titleInput.text?.isEmpty == true {
+      self.dismiss(animated: true, completion: nil)
+      return
+    }
+
+    let yes = UIAlertAction(title: "작성 취소", style: .destructive) { _ in
+      self.dismiss(animated: true, completion: nil)
+    }
+    let no = UIAlertAction(title: "계속 작성", style: .default) { _ in
+      self.titleInput.becomeFirstResponder()
+    }
+
+    let alertController = UIAlertController(
+      title: "앗!",
+      message: "취소하면 작성중인 내용이 손실됩니다.\n취소하시겠어요?",
+      preferredStyle: .alert
+    )
+    alertController.addAction(yes)
+    alertController.addAction(no)
+    self.present(alertController, animated: true, completion: nil)
+  }
+
+  @IBAction func doneButtonDidTap() {
+    guard let title = self.titleInput.text, !title.isEmpty else {
+      self.shakeTitleInput()
+      return
+    }
+
+    self.titleInput.resignFirstResponder()
+
+    let newTask = Task(title: title, note: self.textView.text)
+    self.didAddHandler?(newTask)
+    self.dismiss(animated: true, completion: nil)
+  }
+
+  func shakeTitleInput() {
+    UIView.animate(withDuration: 0.05, animations: { self.titleInput.frame.origin.x -= 5 }, completion: { _ in
+      UIView.animate(withDuration: 0.05, animations: { self.titleInput.frame.origin.x += 10 }, completion: { _ in
+        UIView.animate(withDuration: 0.05, animations: { self.titleInput.frame.origin.x -= 10 }, completion: { _ in
+          UIView.animate(withDuration: 0.05, animations: { self.titleInput.frame.origin.x += 10 }, completion: { _ in
+            UIView.animate(withDuration: 0.05, animations: { self.titleInput.frame.origin.x -= 5 })
+          })
+        })
+      })
+    })
+  }
+
+}
